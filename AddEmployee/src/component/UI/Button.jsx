ed9cf40826588882d53c1481aafccb1a468abefe
Furkan Öztürk <furkan25ozturk@gmail.com>
2024-03-02 23:37:52 +0300

    UseState Operation

--- AddEmployee/src/component/UI/Button.jsx
+++ AddEmployee/src/component/UI/Button.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <button className='p-2 text-lg bg-blue-700 text-white '> {props.children}</button>
+    <button className='p-2 text-lg bg-blue-700 text-white '  
+    type={props.type || "button"}
+    > 
+    {props.children}
+    </button>
   )
 }
 
--- AddEmployee/src/component/Workers/AddWorker.jsx
+++ AddEmployee/src/component/Workers/AddWorker.jsx
@@ -1,21 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../UI/Card'
 import Button from '../UI/Button'
 
 
 function AddWorker() {
+
+   const [enteredWorkerName , setEnteredWorkerName] = useState("");
+   const [enteredWage , setEnteredWage] = useState("");
+
+   const minimumWage = 17000;
+
+   const addWorkerHandler =(e) => {
+    e.preventDefault();// sayfa yenilenmesini engelleme
+
+    if(enteredWorkerName.trim().length ===0 || enteredWage.trim().length ===0 ) {
+      return;
+    }
+
+    if (+enteredWage < minimumWage) {
+      return;
+    }
+    setEnteredWage("");
+      setEnteredWorkerName("");
+     console.log(enteredWorkerName,enteredWage)
+    
+   }
+   
+
+
   return (
     <Card >
-      <form className='flex flex-col gap-y-2'>
+      <form className='flex flex-col gap-y-2' onSubmit={addWorkerHandler}>
         <label htmlFor="name"className='font-medium'>Çalışan İsmi</label>
 
-        <input type="text" className='max-w-[40rem] w-full mx-auto border p-2' placeholder='Çalışan ismi giriniz' id='name'/>
+        <input type="text" className='max-w-[40rem] w-full mx-auto border p-2' placeholder='Çalışan ismi giriniz' id='name' onChange={(e) => setEnteredWorkerName(e.target.value)}
+        value={enteredWorkerName}/>
+        
 
         <label htmlFor="wage" className='font-medium'> Maaş Miktarı</label>
 
-        <input type="number" className='max-w-[40rem] w-full mx-auto border p-2'placeholder='Maaş miktarı giriniz' id='wage' />
+        <input type="number" className='max-w-[40rem] w-full mx-auto border p-2'placeholder='Maaş miktarı giriniz' id='wage' onChange={(e) => setEnteredWage(e.target.value)}
+        value={enteredWage}/>
 
-        <Button className="mt-2"> Ekle</Button>
+        <Button className="mt-2" type="submit"> Ekle</Button>
+        
     </form>
     </Card>
   )
