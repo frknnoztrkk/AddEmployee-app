348500d86d1dfa64cd793119f50cba1569845fe0
Furkan Öztürk <furkan25ozturk@gmail.com>
2024-03-02 19:21:33 +0300

    Card.jsx

--- AddEmployee/src/App.jsx
+++ AddEmployee/src/App.jsx
@@ -4,7 +4,10 @@ import AddWorker from './component/Workers/AddWorker'
 function App() {
   return (
     <div className='App'>
-      <AddWorker/>
+      <h1 className='text-white text-center mt-6 text-3xl'>Maaş Otomasyonu</h1>
+      <div className='mt-10'>
+        <AddWorker/>
+        </div>
     </div>
   )
 }
--- AddEmployee/src/component/Workers/AddWorker.jsx
+++ AddEmployee/src/component/Workers/AddWorker.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import Card from '../UI/Card'
+
 
 function AddWorker() {
   return (
-    <form className='flex flex-col gap-y-2'>
+    <Card >
+      <form className='flex flex-col gap-y-2'>
         <label htmlFor="name"className='font-medium'>Çalışan İsmi</label>
 
         <input type="text" className='max-w-[40rem] w-full mx-auto border p-2' placeholder='Çalışan ismi giriniz' id='name'/>
 
         <label htmlFor="wage" className='font-medium'> Maaş Miktarı</label>
 
-        <input type="text" className='max-w-[40rem] w-full mx-auto border p-2'placeholder='Maaş miktarı giriniz' id='wage' />
+        <input type="number" className='max-w-[40rem] w-full mx-auto border p-2'placeholder='Maaş miktarı giriniz' id='wage' />
 
         <button className='p-2 text-lg bg-teal-700 text-white mt-2'> Ekle</button>
     </form>
+    </Card>
   )
 }
 
