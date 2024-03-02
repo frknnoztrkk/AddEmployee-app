09a43708e1961f13c0a825075ee9e91a4e1cff3b
Furkan Öztürk <furkan25ozturk@gmail.com>
2024-03-02 18:43:40 +0300

    Add Employee

---
+++ AddEmployee/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddWorker from './component/Workers/AddWorker'
+
+function App() {
+  return (
+    <div className='App'>
+      <AddWorker/>
+    </div>
+  )
+}
+
+export default App
